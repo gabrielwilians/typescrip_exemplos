ab5d370f3da04980fa8717cb77775260de97688e
gabriel823 <disasembleme@gmail.com>
2021-10-17 08:41:41 -0300

    aula 03

--- aula 03/consolelog.js
+++ aula 03/consolelog.js
@@ -1,2 +1,13 @@
 console.log('texto literal');
 // ponto e virgula é opcional
+console.log(`craze`);
+console.log("aspas duplas");
+console.log('aspas simples'); //string
+console.log(123456);
+console.log(123.456);
+console.log(123+456, 4654, 98798);
+console.log('meu nome é', "gabriel", "estou aprendendo javascript ás ", 10, " da manhã.");
+
+
+/*comentário
+multipart ou de bloco*/
